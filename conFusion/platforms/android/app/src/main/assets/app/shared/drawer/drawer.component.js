852936f5ad145d1d7040551a26ece2a4234c3bb9
Bryce Capodieci <brycecapodieci@gmail.com>
2018-02-26 14:23:55 -0500

    Side Drawer

---
+++ conFusion/platforms/android/app/src/main/assets/app/shared/drawer/drawer.component.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var DrawerComponent = /** @class */ (function () {
+    function DrawerComponent() {
+    }
+    DrawerComponent = __decorate([
+        core_1.Component({
+            selector: 'drawer-content',
+            templateUrl: './shared/drawer/drawer.component.html',
+        }),
+        __metadata("design:paramtypes", [])
+    ], DrawerComponent);
+    return DrawerComponent;
+}());
+exports.DrawerComponent = DrawerComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZHJhd2VyLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImRyYXdlci5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxzQ0FBd0M7QUFNeEM7SUFFSTtJQUNBLENBQUM7SUFIUSxlQUFlO1FBSjNCLGdCQUFTLENBQUM7WUFDUCxRQUFRLEVBQUUsZ0JBQWdCO1lBQzFCLFdBQVcsRUFBRSx1Q0FBdUM7U0FDdkQsQ0FBQzs7T0FDVyxlQUFlLENBSTNCO0lBQUQsc0JBQUM7Q0FBQSxBQUpELElBSUM7QUFKWSwwQ0FBZSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q29tcG9uZW50fSBmcm9tIFwiQGFuZ3VsYXIvY29yZVwiO1xyXG5cclxuQENvbXBvbmVudCh7XHJcbiAgICBzZWxlY3RvcjogJ2RyYXdlci1jb250ZW50JyxcclxuICAgIHRlbXBsYXRlVXJsOiAnLi9zaGFyZWQvZHJhd2VyL2RyYXdlci5jb21wb25lbnQuaHRtbCcsXHJcbn0pXHJcbmV4cG9ydCBjbGFzcyBEcmF3ZXJDb21wb25lbnQge1xyXG5cclxuICAgIGNvbnN0cnVjdG9yKCkge1xyXG4gICAgfVxyXG59Il19
