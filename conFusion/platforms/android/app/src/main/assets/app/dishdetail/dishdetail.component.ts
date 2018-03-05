31218d112e9207284c87f5dc341b2f76e994d818
Bryce Capodieci <brycecapodieci@gmail.com>
2018-03-04 21:15:01 -0500

    Assignment 2 NativeScript UI Elements

--- conFusion/platforms/android/app/src/main/assets/app/dishdetail/dishdetail.component.ts
+++ conFusion/platforms/android/app/src/main/assets/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewContainerRef } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 
@@ -10,6 +10,10 @@ import { RouterExtensions } from 'nativescript-angular/router';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { Toasty } from 'nativescript-toasty';
 import 'rxjs/add/operator/switchMap';
+
+import { action } from "tns-core-modules/ui/dialogs";
+import { ModalDialogOptions, ModalDialogService } from "nativescript-angular";
+import { CommentComponent } from "../comment/comment.component";
 
 @Component({
   selector: 'app-dishdetail',
@@ -25,12 +29,15 @@ export class DishdetailComponent implements OnInit {
   avgstars: string;
   numcomments: number;
   favorite: boolean = false;
+  showComments: boolean = false;
 
   constructor(private dishservice: DishService,
     private favoriteservice: FavoriteService,
     private fonticon: TNSFontIconService, 
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
+    private modalService: ModalDialogService,
+    private vcRef: ViewContainerRef,
     @Inject('BaseURL') private BaseURL) { }
 
     ngOnInit() {
@@ -60,4 +67,49 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
     this.routerExtensions.back();
   }
+
+
+showDialog(): void {
+  let options = {
+      title: "Actions",
+      message: "Select an Action",
+      cancelButtonText: "Cancel",
+      actions: ["Add to Favorites", "Add Comment"]
+  };
+
+  action(options).then((result) => {
+      console.log(result);
+      switch(result) {
+          case "Add to Favorites":
+              this.addToFavorites();
+              break;
+          case "Add Comment":
+              this.showCommentModalForm();
+              break;
+      }
+  });
+}
+
+showCommentModalForm(): void {
+  let options: ModalDialogOptions = {
+      viewContainerRef: this.vcRef,
+      fullscreen: false
+  };
+  this.modalService.showModal(CommentComponent, options)
+      .then((result: any) => {
+          const d = new Date();
+          const n = d.toISOString();
+          this.comment = {
+              author: result.author,
+              comment: result.comment,
+              rating: result.rating,
+              date: n
+          };
+          let total = 0;
+          this.dish.comments.push(this.comment);
+          this.numcomments = this.dish.comments.length;
+          this.dish.comments.forEach(comment => total += comment.rating);
+          this.avgstars = (total/this.numcomments).toFixed(2);
+      });
+}
 }
