3546c1e9a3cbb8fee60ef52cd8187edb66b09255
Bryce Capodieci <brycecapodieci@gmail.com>
2018-03-06 21:55:14 -0500

    Animations and Gestures Part 1

--- conFusion/platforms/android/app/src/main/assets/app/services/favorite.service.ts
+++ conFusion/platforms/android/app/src/main/assets/app/services/favorite.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
+
 import { DishService } from '../services/dish.service';
+import { CouchbaseService } from '../services/couchbase.service';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -8,9 +11,19 @@ import 'rxjs/add/operator/map';
 export class FavoriteService {
 
     favorites: Array<number>;
+    docId: string = "favorites";
 
-    constructor(private dishservice: DishService) {
+    constructor(private dishservice: DishService,
+        private couchbaseservice: CouchbaseService) {
         this.favorites = [];
+
+        let doc = this.couchbaseservice.getDocument(this.docId);
+        if (doc == null) {
+            this.couchbaseservice.createDocument({"favorites": []}, this.docId);
+        }
+        else {
+            this.favorites = doc.favorites;
+        }
     }
 
     isFavorite(id: number): boolean {
@@ -20,6 +33,7 @@ export class FavoriteService {
     addFavorite(id: number): boolean {
         if(!this.isFavorite(id)) {
             this.favorites.push(id);
+            this.couchbaseservice.updateDocument(this.docId, {"favorites": this.favorites});
         }
         return true;
     }
@@ -33,6 +47,7 @@ export class FavoriteService {
         let index = this.favorites.indexOf(id);
         if (index >= 0) {
             this.favorites.splice(index,1);
+            this.couchbaseservice.updateDocument(this.docId, {"favorites": this.favorites});
             return this.getFavorites();
         }
         else {
